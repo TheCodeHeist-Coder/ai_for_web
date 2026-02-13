64ed630ceb108af8d7573adb3b33202df9114295
Rajkumar <thecodeheist01@gmail.com>
2026-02-13 17:08:38 +0530

    chat_history_added

--- app.js
+++ app.js
@@ -1,23 +1,65 @@
+// import { GoogleGenAI } from "@google/genai";
+// import { configDotenv } from "dotenv";
+// configDotenv()
+
+// const ai = new GoogleGenAI({});
+
+// async function main() {
+//   const response = await ai.models.generateContent({
+//     model: "gemini-3-flash-preview",
+//     config:{
+//        systemInstruction: `You are a coding tutor,
+//        Strict rules to follow
+//         - You will only answer the questions which is related to coding
+//         - Don't answer anything which is not related to coding,
+//         - Reply rudely to user if they ask question which is not related to coding,
+//         EX:  You dumb, only ask questions related to coding`
+//     },
+//     contents: "Who is the prime-minister of india",
+//   });
+//   console.log(response.text);
+// }
+
+// main();
+
+
+
+//! Multi-turn conversations
 import { GoogleGenAI } from "@google/genai";
+import readlineSync from 'readline-sync'
 import { configDotenv } from "dotenv";
 configDotenv()
 
 const ai = new GoogleGenAI({});
 
 async function main() {
-  const response = await ai.models.generateContent({
+  const chat = ai.chats.create({
     model: "gemini-3-flash-preview",
-    config:{
-       systemInstruction: `You are a coding tutor,
-       Strict rules to follow
-        - You will only answer the questions which is related to coding
-        - Don't answer anything which is not related to coding,
-        - Reply rudely to user if they ask question which is not related to coding,
-        EX:  You dumb, only ask questions related to coding`
-    },
-    contents: "Who is the prime-minister of india",
+    history: [],
+    config: {
+      systemInstruction: `You are a coding tutor,
+         Strict rules to follow
+         - You will only answer the questions which is related to coding
+         - Don't answer anything which is not related to coding,
+         - Reply rudely to user if they ask question which is not related to coding,
+         EX:  You dumb, only ask questions related to coding`
+    }
   });
-  console.log(response.text);
+
+  while(true){
+    const question = readlineSync.question("Ask me question: ")
+
+    if(question == 'exit'){
+      break;
+    }
+
+    const response = await chat.sendMessage({
+      message: question
+    })
+
+    console.log("Answer: ", response.text)
+  }
+
 }
 
-main();
+await main();
